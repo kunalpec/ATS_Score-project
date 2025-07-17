f3008398c28663f9f961a5fbee1dd7dd08fb809c
kunalpec <kunal.bt23cseds@pec.edu.in>
2025-07-17 06:20:46 +0530

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ genai.configure(api_key=st.secrets["API_KEY"])
 
 def get_gemini_response(input,pdf_content,prompt):
   model=genai.GenerativeModel('gemini-1.5-flash')
-  response=model.generate_content([input,pdf_content[0],prompt])
+  response=model.generate_content([input,pdf_content,prompt])
   return response
 
 import base64
